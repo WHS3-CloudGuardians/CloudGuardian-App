9594986df0bd56f4f75e4ffeb8b78bb6d5d1b60a
dasol729 <97594634+dasol729@users.noreply.github.com>
2025-06-16 22:43:25 +0900

    refactor directory & file

--- server/config/DBConfig.js
+++ server/config/DBConfig.js
@@ -4,6 +4,6 @@ module.exports = {
   HOST: process.env.DB_HOST || 'localhost',
   USER: process.env.DB_USER || 'root',
   PASSWORD: process.env.DB_PASSWORD || '',
-  DB: process.env.DB_NAME || 'cloudguardian',
+  DB: process.env.DB_NAME || 'test',
   dialect: 'mysql',
 };
--- server/models/Index.js
+++ server/models/Index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const dbConfig = require('../config/db.config');
+const dbConfig = require('../config/DBConfig');
 
 const sequelize = new Sequelize(
   dbConfig.DB,
@@ -16,7 +16,7 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.User = require('./user.model')(sequelize, Sequelize);
+db.User = require('./UserModel')(sequelize, Sequelize);
 
 // 관계 설정
 db.Post.belongsTo(db.User);
