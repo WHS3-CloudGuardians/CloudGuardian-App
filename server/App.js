0ca4569481f6dc8b5818bac66aa6175ba893a500
ye-nni <sohneun22@gmail.com>
2025-06-20 06:35:30 +0900

    Update App.js

--- server/App.js
+++ server/App.js
@@ -1,19 +1,31 @@
 // app.js
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const authRoutes = require("./routes/AuthRoutes");
+const postRoutes = require("./routes/PostRoutes");
 const db = require("./models");
 const { errorHandler } = require("./utils/Response");
 
 const app = express();
 const PORT = 3000;
 
-// ✅ JSON 파서 추가
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+// console.log(process.env.JWT_SECRET);
+
+// JSON 파서 추가
 app.use(express.json());
+app.use("/api", postRoutes);
+app.use("/api/auth", authRoutes);
 
 // 정적 파일 제공 (선택: public 폴더에 CSS/JS 넣을 때)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// console.log('⟡ dotenv.config() →', require('dotenv').config());
 
 // 루트 라우터
 app.get('/', (req, res) => {
@@ -26,9 +38,6 @@ db.sequelize.sync().then(() => {
     console.log(`서버 실행 중: http://localhost:${PORT}`)
   );
 });
-
-// 사용자
-app.use("/api/auth", authRoutes);
 
 // 에러 핸들러 사용
 app.use(errorHandler);
