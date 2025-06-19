6d454269bb153fb868f9fcf0be32249bc61b0d0a
ye-nni <sohneun22@gmail.com>
2025-06-20 06:28:55 +0900

    Update DBConfig.js

--- server/config/DBConfig.js
+++ server/config/DBConfig.js
@@ -6,6 +6,6 @@ module.exports = {
   HOST: process.env.DB_HOST || 'localhost',
   USER: process.env.DB_USER || 'root',
   PASSWORD: process.env.DB_PASSWORD || '',
-  DB: process.env.DB_NAME || 'test',
+  DB: process.env.DB_NAME || 'test', //CloudGuardian
   dialect: 'mysql',
 };
