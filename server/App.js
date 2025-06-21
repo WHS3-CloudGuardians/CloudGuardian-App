b6134bd44f0b6a9b62f7e1b67ff243aafe49d037
dasol729 <97594634+dasol729@users.noreply.github.com>
2025-06-22 01:12:26 +0900

    배포 환경 구성

--- server/App.js
+++ server/App.js
@@ -9,31 +9,24 @@ const db = require("./models");
 const { errorHandler } = require("./utils/Response");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
+// CORS 설정 (개발환경)
 app.use(cors({
   origin: 'http://localhost:5173'
 }));
 
 // 루트 라우터
-app.get('/', (req, res) => {
-  res.send('<h1>Hello, Node.js 앱이 실행 중입니다!</h1>');
-});
-
-// DB 연결 및 서버 실행
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => 
-    console.log(`서버 실행 중: http://localhost:${PORT}`)
-  );
-});
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello, Node.js 앱이 실행 중입니다!</h1>');
+// });
 
 // JSON 파서 추가
 app.use(express.json());
+
+// API 라우터
 app.use("/api", postRoutes);
 app.use("/api/auth", authRoutes);
-
-// 에러 핸들러 사용
-app.use(errorHandler);
 
 // 정적 파일 제공
 // app.use(express.static(path.join(__dirname, 'public')));
@@ -42,4 +35,16 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 // 리액트 라우팅 대응
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+// 에러 핸들러 사용
+app.use(errorHandler);
+
+// DB 연결 및 서버 실행
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => 
+    console.log(`서버 실행 중: http://localhost:${PORT}`)
+  );
+}).catch(err => {
+  console.error("DB 연결 실패:", err);
 });
