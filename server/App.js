bb2ac1edc9ed5a956d9f8700bfc38d9adf0be2ce
dasol729 <97594634+dasol729@users.noreply.github.com>
2025-06-21 23:59:35 +0900

    로컬 변경사항 커밋

--- server/App.js
+++ server/App.js
@@ -1,18 +1,48 @@
 // app.js
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+const authRoutes = require("./routes/AuthRoutes");
+const postRoutes = require("./routes/PostRoutes");
+const db = require("./models");
+const { errorHandler } = require("./utils/Response");
 
 const app = express();
 const PORT = 3000;
 
-// 정적 파일 제공 (선택: public 폴더에 CSS/JS 넣을 때)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+// JSON 파서 추가
+app.use(express.json());
+app.use("/api", postRoutes);
+app.use("/api/auth", authRoutes);
+
+// 정적 파일 제공
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // 루트 라우터
 app.get('/', (req, res) => {
   res.send('<h1>Hello, Node.js 앱이 실행 중입니다!</h1>');
 });
 
+// DB 연결 및 서버 실행
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => 
+    console.log(`서버 실행 중: http://localhost:${PORT}`)
+  );
+});
+
+// 에러 핸들러 사용
+app.use(errorHandler);
+
+// 리액트 라우팅 대응
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 // 서버 실행
 app.listen(PORT, () => {
   console.log(`✅ 서버가 실행되었습니다: http://localhost:${PORT}`);
