ad41d1b42c32e5b35d65cdd9017515858638d979
ye-nni <sohneun22@gmail.com>
2025-06-23 00:19:04 +0900

    Update redis.js

--- server/redis.js
+++ server/redis.js
@@ -2,6 +2,15 @@
 const Redis = require('ioredis');
 const redis = new Redis(process.env.REDIS_URL);
 
+// aws 배포 시
+/* const redis = new Redis({
+  host: process.env.REDIS_HOST,       // ex) "my-cache.xxxxxx.0001.use1.cache.amazonaws.com"
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD,  // (암호화 활성화한 경우)
+  tls: process.env.REDIS_TLS === 'true' ? {} : undefined, // TLS 연결이 필요하면
+});
+ */
+
 redis.on('error', err => {
   console.error('[Redis] error', err);
 });
