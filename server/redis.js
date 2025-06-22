0ab9c70eab299bea21d05b6e3a3eaa1a79da8422
ye-nni <sohneun22@gmail.com>
2025-06-22 23:57:49 +0900

    redis 추가

---
+++ server/redis.js
@@ -0,0 +1,9 @@
+// server/redis.js
+const Redis = require('ioredis');
+const redis = new Redis(process.env.REDIS_URL);
+
+redis.on('error', err => {
+  console.error('[Redis] error', err);
+});
+
+module.exports = redis;
