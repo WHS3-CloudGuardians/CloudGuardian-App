d3a6178fa2c263ad807cadf17407da7b206bc3cd
dasol729 <97594634+dasol729@users.noreply.github.com>
2025-07-03 12:13:45 +0900

    테스트 환경 수정

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // TODO: 실제 백엔드 URL
+  baseURL: '/api', // TODO: 실제 백엔드 URL
   withCredentials: true, // 쿠키 전송 허용
 });
 
